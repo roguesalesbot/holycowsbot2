3b3bb599f53eb82ffeac3e6b273e780187c3203b
luho <luho@luhos-mbp.myfiosgateway.com>
2022-03-21 17:50:56 -0400

    rev it up

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function formatAndSendTweet(event) {
 
 // Poll OpenSea every 300 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(269, "seconds").unix();
+    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(61, "seconds").unix();
 
     console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
@@ -77,4 +77,4 @@ setInterval(() => {
     }).catch((error) => {
         console.error(error);
     });    
-}, 300000);
+}, 60000);
